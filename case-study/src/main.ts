5d927d73c76bc473e8142e217e9dd1aadf1821cf
wahyudi.indra <wahyudiindra79@gmail.com>
2025-09-18 17:57:19 +0700

    chore(case study): set swagger docs and adjusment main.ts

--- case-study/src/main.ts
+++ case-study/src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
-    await app.listen(process.env.PORT ?? 3000);
+
+    const options = new DocumentBuilder()
+        .setTitle('Case Study API')
+        .setVersion('1.0')
+        .addBearerAuth()
+        .setExternalDoc('Case Study - Collection', '/docs-json')
+        .build();
+    const document = SwaggerModule.createDocument(app, options);
+    SwaggerModule.setup('docs', app, document);
+
+    const host = process.env.HOST || 'localhost';
+    const port = process.env.PORT || 4000;
+
+    await app.listen(port as number, host);
+    console.info(`Server running on ${host}:${port}`);
 }
 bootstrap();
